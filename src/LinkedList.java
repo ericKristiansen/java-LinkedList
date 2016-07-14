96f837955988acfa6cec3e50914d8a96805388b3
Eric Kristiansen <eric.kristiansen@mail.com>
2016-07-14 13:23:29 -0700

    set up

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -9,31 +9,47 @@ public class LinkedList<AnyType>
     private Node<AnyType> head;
     private Node<AnyType> tail;
 
-    public LinkedList()
-    {
-        head = tail = null;
-    }
 
+    /**
+     * Fetch the head of the list.
+     * @return head
+     */
     public Node<AnyType> getHead()
     {
         return head;
     }
 
+    /**
+     * Fetch the tail of the list.
+     * @return tail
+     */
     public Node<AnyType> getTail()
     {
         return head;
     }
 
+    /**
+     * Assign the head of the list.
+     * @param passHead
+     */
     public void setHead(Node<AnyType> passHead)
     {
         head = passHead;
     }
 
+    /**
+     * Assign the tail of the list.
+     * @param passTail
+     */
     public void setTail(Node<AnyType> passTail)
     {
         tail = passTail;
     }
 
+    /**
+     *  Remove the head from the list.
+     * @return head
+     */
     public Node<AnyType>RemoveHead()
     {
         head = head.getNext();
--- src/Node.java
+++ src/Node.java
@@ -6,44 +6,78 @@ public class Node<AnyType>
     private AnyType data;
     private Node<AnyType> next;
 
-    public Node()
-    {
-        data = null;
-        next = null;
-    }
 
+    /**
+     * Constructor taking one parameter
+     * @param passData
+     */
     public Node(AnyType passData)
     {
         this.data = passData;
-        next = null;
     }
 
+    /**
+     * Constructor taking two parameters
+     * @param passData
+     * @param passNext
+     */
     public Node(AnyType passData, Node<AnyType> passNext)
     {
         data = passData;
         next = passNext;
     }
 
+    /**
+     * Copy Constructor
+     * @param passNode
+     */
+    public Node(Node<AnyType> passNode)
+    {
+        this.data = passNode.getData();
+        this.next = passNode.getNext();
+    }
+
+    /**
+     * Fetch data.
+     * @return data
+     */
     public AnyType getData()
     {
         return data;
     }
 
+    /**
+     * Fetch next.
+     * @return next
+     */
     public Node<AnyType> getNext()
     {
         return next;
     }
 
+    /**
+     * Set the next Node.
+     * @param passNode
+     */
     public void setNext(Node<AnyType> passNode)
     {
         next = passNode;
     }
 
+    /**
+     * Set data.
+     * @param passNode
+     */
     public void setData(Node<AnyType> passNode)
     {
         data = passNode.getData();
     }
 
+    /**
+     * Copy passNode into this one, and return this Node.
+     * @param passNode
+     * @return this
+     */
     public Node<AnyType> copyNode(Node<AnyType> passNode)
     {
         if (this != passNode)
